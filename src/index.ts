b66a87a401a9df891fc5ad16acd5c924cbc1d143
Anton <popsof@yandex.ru>
2024-09-07 00:58:54 +0300

    index file upd

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,26 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import { bot } from './conf/StartBotConfig'; // Ваш бот
+import { bot } from './conf/StartBotConfig';
 
-dotenv.config(); // Инициализация dotenv
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Основной маршрут
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// Запуск сервера
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-// Запуск бота
 bot.launch().then(() => console.log('Bot is running'));
 
-// Обработка завершения работы
 process.on('SIGINT', () => {
   console.log('Shutting down...');
   bot.stop('SIGINT');
   process.exit(0);
 });
 
-// Команды бота
-import './controller/BotController'; // Импорт команд и обработчиков бота
+import './controller/BotController';
